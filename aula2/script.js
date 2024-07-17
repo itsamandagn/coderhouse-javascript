e0c6ed3ebf6c06e21386e9e3df6cfb4b43526bc9
Amanda <amandagn07@gmail.com>
2024-07-16 21:56:21 -0300

    variaveis prompt e alert

---
+++ aula2/script.js
@@ -0,0 +1,16 @@
+alert("Olá mundo")
+
+var nome = "Amanda";
+var sobrenome = "Guimaraes";
+var anoAtual = 2024;
+var idadeFinal = 29;
+var resultadoA= nome + sobrenome;
+var resultadoB= anoAtual - idadeFinal;
+
+console.log(resultadoA);
+console.log(resultadoB);
+
+let valor1 = 1;
+var valor2 =  4;
+var resultado = valor1 + valor2;
+console.log(resultado);
