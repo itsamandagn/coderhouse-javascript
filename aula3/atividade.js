3eef8c85034cc6ecf38d9f50b9662dd6fab098e4
Amanda <amandagn07@gmail.com>
2024-07-22 19:12:49 -0300

    atividade para casa

---
+++ aula3/atividade.js
@@ -0,0 +1,14 @@
+var num = prompt("Insira um número");
+num = parseInt(num);
+if (num >= 1000)
+alert("o número inserido é maior que 1000");
+else if (num < 1000)
+alert("o número inserido é menor que 1000");
+
+
+var valor = prompt("favor insira um número qualquer");
+valor = parseInt(valor);
+if (valor >=10 && valor <=50)
+alert("o número inserido se encontra no intervalo de 10 a 50");
+else if (num <10 || num >50)
+alert ("o número NÃO está no intervalo de 1 a 50");
