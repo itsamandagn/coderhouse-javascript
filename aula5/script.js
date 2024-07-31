c235f43cdafed1d495494699f3a92ce831f3574f
Amanda <amandagn07@gmail.com>
2024-07-30 21:50:12 -0300

    treinando looping

---
+++ aula5/script.js
@@ -0,0 +1,7 @@
+let multiplo = (num1,num2) => { 
+    if (num1 % num2 === 0)
+   return "os números são multiplos";
+   else return ("os números NÃO são multiplos");}
+   
+   console.log (multiplo(20,2));
+   console.log (multiplo(15,7));
---
+++ aula6/script.js
@@ -0,0 +1,19 @@
+/*let numero = parseInt(prompt("insira um número de 1 a 10"));
+function exibir(numero){
+if (numero>10) return console.log("número maior que 10");
+for (let i=0;i<numero;i++)
+    console.log (i);
+  }
+exibir(numero);*/
+
+function fatorial(numero) {
+    let i = 1;
+    resultado = i;
+    while(i<=numero){
+        resultado = resultado*i;
+        console.log(i)
+        i++
+    }
+    return resultado;
+}
+fatorial(4);
